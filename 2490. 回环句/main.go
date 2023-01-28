3cf30cebec8b8e6c1417ab9009e82f713ef42b7a
xingming.huo <huoxingming@singularity-ai.com>
2023-01-28 16:07:34 +0800

    add 2490. 回环句

---
+++ 2490. 回环句/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func isCircularSentence(sentence string) bool {
+	var i int
+	var s uint8
+	for i < len(sentence) {
+		for i < len(sentence) && sentence[i] != ' ' {
+			s = sentence[i]
+			i++
+		}
+		for i < len(sentence) && sentence[i] == ' ' {
+			i++
+		}
+		if i < len(sentence) && sentence[i] != s {
+			return false
+		}
+	}
+	return sentence[0] == sentence[len(sentence)-1]
+}
+
+func main() {
+	for _, s := range []string{
+		"abcbcb ba",
+		"abcb bc",
+		"abccb baaaa",
+		"abccbbaaaa",
+	} {
+		fmt.Println(s, isCircularSentence(s))
+	}
+}
